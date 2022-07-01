e4888c8bdefb8f78dcd07bf00c30fb3ff191006c
Edgar <jurirulez@gmail.com>
2022-07-01 15:20:29 +0300

    final

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,11 @@
-// document.querySelectorAll(".document-type__checkbox").forEach((o) => {
-//   o.addEventListener("click", (e) => {
-//     const { checked } = e.target;
-
-//     if (checked) {
-//       let parent = o.closest(".document-type");
-//       parent.classList.add("document-type_checked-active");
-//     }
-//   });
-// });
-
-// function onHandleRemoveActiveClass() {
-//   document.querySelectorAll(".document-type").forEach((a) => {
-//     a.classList.remove("document-type_checked-active");
-//   });
-// }
+const mixerDocuments = mixitup(".upload-document__container", {
+  selectors: {
+    target: ".upload-document__img-wrapper",
+  },
+  animation: {
+    duration: 300,
+  },
+  load: {
+    filter: ".category-1",
+  },
+});
